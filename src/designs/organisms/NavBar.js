e0f6f1ba00dcfc207525356ffe7cc8425e8264bb
vinny <vincentmarcos@hotmail.com>
2018-02-16 16:15:37 +0800

    change to mobile

--- src/designs/organisms/NavBar.js
+++ src/designs/organisms/NavBar.js
@@ -1,18 +1,48 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
-const logo = require('./../../photos/NavBar/logo.jpg')
+import IconButton from 'material-ui/IconButton';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/menu';
+const logo = require('./../../assets/photos/NavBar/logo.jpg')
 const NavBar = () => (
   <div className='nav-bar'>
     <div className="logo"><img src={logo} alt="inframe-logo"/></div>
-    <div className="links">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/portfolio">Portfolio</Link></li>
-        <li><Link to="/services">Services</Link></li>
-        <li><Link to="/process">Process</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
-        <li><Link to="/jobs">Jobs</Link></li>
-    </div>
+    <IconMenu
+          iconButtonElement={<IconButton style={{height: '50px', width: '50px'}}><MoreVertIcon color={'white'}/></IconButton>}
+        >
+        <MenuItem href='/' primaryText='Home'/>
+        <MenuItem href='/portfolio' primaryText='Portfolio'/>
+        <MenuItem href='/services' primaryText='Services'/>
+        <MenuItem href='/process' primaryText='Process'/>
+        <MenuItem href='/about' primaryText='About'/>
+        <MenuItem href='/contact' primaryText='Contact'/>
+        <MenuItem href='/jobs' primaryText='Jobs'/>
+        </IconMenu>
+    {/* <div className="links">
+        <li>
+        <Link to="</Link>
+        <MenuItem href='/">Home' />
+        </li>
+        <li>
+        <Link to="">Portfolio</Link>
+        </li>
+        <li>
+        <Link to="">Services</Link>
+        </li>
+        <li>
+        <Link to="">Process</Link>
+        </li>
+        <li>
+        <Link to="">About</Link>
+        </li>
+        <li>
+        <Link to="">Contact</Link>
+        </li>
+        <li>
+        <Link to="">Jobs</Link>
+        </li>
+    </div> */}
   </div>
 );
 
--- src/designs/pages/HomePage.js
+++ src/designs/pages/HomePage.js
@@ -18,98 +18,132 @@ const surfer2 = require("../../assets/photos/Portfolio/surf/2.jpg")
 const surfer3 = require("../../assets/photos/Portfolio/surf/3.jpg")
 
 // const URL = process.env.REACT_APP_PUBLIC_URL
+const animals = [
+  {
+    src: animal1,
+    altText: 'Slide 1',
+    caption: 'Slide 1'
+  },
+  {
+    src: animal2,
+    altText: 'Slide 2',
+    caption: 'Slide 2'
+  },
+  {
+    src: animal3,
+    altText: 'Slide 3',
+    caption: 'Slide 3'
+  }
+]
+
+const bodyBuilders = [
+  {
+    src: bodyBuilder1,
+    altText: 'Slide 1',
+    caption: 'Slide 1'
+  },
+  {
+    src: bodyBuilder2,
+    altText: 'Slide 2',
+    caption: 'Slide 2'
+  },
+  {
+    src: bodyBuilder3,
+    altText: 'Slide 3',
+    caption: 'Slide 3'
+  }
+]
+
+const surfers = [
+  {
+    src: surfer1,
+    altText: 'Slide 1',
+    caption: 'Slide 1'
+  },
+  {
+    src: surfer2,
+    altText: 'Slide 2',
+    caption: 'Slide 2'
+  },
+  {
+    src: surfer3,
+    altText: 'Slide 3',
+    caption: 'Slide 3'
+  }
+]
+
+    const imageGroups = [surfers,animals,bodyBuilders]
+
+    const carousels = imageGroups.map((imageGroup,index) =>
+    {
+      const slides = imageGroup.map((imageData,index) =>
+      {
+        const className = (index===0) ? ('carousel-item active') : ('carousel-item')
+        return (
+          <div class={className}>
+          <img class="d-block w-100 " src={imageData.src} alt={imageData.altText} />
+        </div>
+        )}
+    )
+      return (
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+        {slides}
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+      </a>
+    </div>
+  )
+  }
+);
 
 class HomePage extends Component {
   render() {
 
-    const animals = [
-      {
-        src: animal1,
-        altText: 'Slide 1',
-        caption: 'Slide 1'
-      },
-      {
-        src: animal2,
-        altText: 'Slide 2',
-        caption: 'Slide 2'
-      },
-      {
-        src: animal3,
-        altText: 'Slide 3',
-        caption: 'Slide 3'
-      }
-    ]
-
-    const bodyBuilders = [
-      {
-        src: bodyBuilder1,
-        altText: 'Slide 1',
-        caption: 'Slide 1'
-      },
-      {
-        src: bodyBuilder2,
-        altText: 'Slide 2',
-        caption: 'Slide 2'
-      },
-      {
-        src: bodyBuilder3,
-        altText: 'Slide 3',
-        caption: 'Slide 3'
-      }
-    ]
-
-    const surfers = [
-      {
-        src: surfer1,
-        altText: 'Slide 1',
-        caption: 'Slide 1'
-      },
-      {
-        src: surfer2,
-        altText: 'Slide 2',
-        caption: 'Slide 2'
-      },
-      {
-        src: surfer3,
-        altText: 'Slide 3',
-        caption: 'Slide 3'
-      }
-    ]
-
     return (
-      [<div className="splash-screen">
-        <div className="splash-banner">
-          <div className="splash-image">
+      [
+        <div className="splash-screen">
+          <div className="splash-banner">
+            <div className="splash-image">
+            </div>
+            <div className='over-image'>
+              <h2><b>Let us tell your story</b></h2>
+              <Button className='demo-reel-button' secondary={true} label='SEE OUR DEMO REEL'/>
+            </div>
           </div>
-          <div className='over-image'>
-            <h2><b>Let us tell your story</b></h2>
-            <Button className='demo-reel-button' secondary={true} label='SEE OUR DEMO REEL'/>
+          <div className="splash-text">
+            <p>InFrame Media Productions is a full service video production company and creative agency specializing in corporate, marketing, training, event and web viral videos located in Gold Coast Australia.</p>
           </div>
         </div>
-        <div className="splash-text">
-          <p>InFrame Media Productions is a full service video production company and creative agency specializing in corporate, marketing, training, event and web viral videos located in Gold Coast Australia.</p>
-        </div>
+        ,
+        <div>{carousels}</div>
+      ,
+      <div className="carousels">
+      <div className="home-portfoios">
+      <h2>Production Portfolio</h2>
+      <div className="slideshow-container">
+      <Slider items={animals}/>
+      </div>
       </div>,
-        <div className="carousels">
-          <div className="home-portfoios">
-          <h2>Production Portfolio</h2>
-            <div className="slideshow-container">
-              <Slider items={animals}/>
-            </div>
-          </div>,
-          <div className="home-portfoios">
-          <h2>Production Portfolio</h2>
-            <div className="slideshow-container">
-              <Slider items={bodyBuilders}/>
-            </div>
-          </div>,
-          <div className="home-portfoios">
-          <h2>Production Portfolio</h2>
-            <div className="slideshow-container">
-              <Slider items={surfers}/>
-            </div>
-          </div>
-        </div>]
+      <div className="home-portfoios">
+      <h2>Production Portfolio</h2>
+      <div className="slideshow-container">
+      <Slider items={bodyBuilders}/>
+      </div>
+      </div>,
+      <div className="home-portfoios">
+      <h2>Production Portfolio</h2>
+      <div className="slideshow-container">
+      <Slider items={surfers}/>
+      </div>
+      </div>
+      </div>]
     )
   }
 }
